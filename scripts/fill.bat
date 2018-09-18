959dafc4fe4e12cd0b97e9d283ba280bedf91cbb
LetsCode-Angus <38348730+LetsCode-Angus@users.noreply.github.com>
2018-09-18 19:42:52 +1000

    Updated gitignore

--- scripts/fill.bat
+++ scripts/fill.bat
@@ -1,9 +1,12 @@
 @echo off
 IF "%1" == "/?" GOTO :help
+
 :main
 echo FILLING "%1" WITH %2 BYTES OF DATA
 FOR /L %%A IN (1,1, %2) DO (
+  ::The set /p param ensures that a new line does not get added
   echo|set /p="0" >> %1
 )
+
 :help
 echo To use this command: fill [filename] [num of bytes]
