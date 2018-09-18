d45b71e28d57a84ceb73554a8a56f38ba1b47b29
LetsCode-Angus <38348730+LetsCode-Angus@users.noreply.github.com>
2018-09-18 19:23:27 +1000

    Updates

--- scripts/fill.bat
+++ scripts/fill.bat
@@ -1,4 +1,9 @@
 @echo off
+IF "%1" == "/?" GOTO :help
 :main
-echo %random%%random%%random%%random%%random%%random%%random%%random% >> %1
-goto :main
+echo FILLING "%1" WITH %2 BYTES OF DATA
+FOR /L %%A IN (1,1, %2) DO (
+  echo|set /p="0" >> %1
+)
+:help
+echo To use this command: fill [filename] [num of bytes]
