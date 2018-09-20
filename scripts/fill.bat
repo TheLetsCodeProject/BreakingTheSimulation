fa06d78400a47fb10dc6c56c727e705ddd3a5686
Angus B <38348730+LetsCode-Angus@users.noreply.github.com>
2018-09-20 10:20:20 +1000

    Update fill.bat

--- scripts/fill.bat
+++ scripts/fill.bat
@@ -1,12 +1,19 @@
 @echo off
 IF "%1" == "/?" GOTO :help
 
+::Variable declarations
+set byte = 1
+set /a kb = %byte% * 1024
+set /a mb = %kb% * 1024
+set /a gb = %mb% * 1024
+
+set /a amount = %1 * %%3%
 :main
-echo FILLING "%1" WITH %2 BYTES OF DATA
-FOR /L %%A IN (1,1, %2) DO (
+echo FILLING "%1" WITH %2 %3 OF DATA
+FOR /L %%A IN (1,1, %amount%) DO (
   ::The set /p param ensures that a new line does not get added
   echo|set /p="0" >> %1
 )
 
 :help
-echo To use this command: fill [filename] [num of bytes]
+echo To use this command: fill [filename] [num of bytes] [unit]
